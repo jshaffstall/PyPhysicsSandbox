4914970264d54f8e663eec863eba379c85043e8a
jshaffstall <jshaffstall@gmail.com>
2016-11-24 21:37:19 -0500

    Minor changes to catapult for effect

--- Catapult.py
+++ Catapult.py
@@ -17,18 +17,24 @@ triangle.color = Color("green")
 lever = poly(((50, 425), (65, 425), (65, 440), (450, 440), (450, 450), (50, 450)))
 #lever = poly(((50, 200), (50, 450), (450, 450), (450, 440), (65, 440), (65, 200)))
 lever.color = Color("darkblue")
+lever.elasticity = 0.50
 
 ball
 ball1 = ball( (90, 425), 10)
-ball1.color = Color("darkgreen")
+ball1.color = Color("green")
+ball1.wrap = True
 
 # bigger ball, initially off the top of the screen
 ball2 = ball( (425, -1000), 20)
 ball2.color = Color("darkgreen")
 
+# bigger ball, farther off the top of the screen
+ball3 = ball( (425, -5000), 20)
+ball3.color = Color("blue")
+
+
 text
 text = text ((90,250), "Catapult")
-text.wrap = True
 
 run()
 
