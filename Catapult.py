321759c238cda44247b32f296b12e34194f9c495
jshaffstall <jshaffstall@gmail.com>
2016-11-25 19:35:11 -0500

    Catapult changes

--- Catapult.py
+++ Catapult.py
@@ -14,7 +14,7 @@ triangle = poly(((250, 450), (275, 500), (225, 500)))
 triangle.color = Color("green")
 
 # lever
-lever = poly(((50, 425), (65, 425), (65, 440), (450, 440), (450, 450), (50, 450)))
+lever = poly(((50, 405), (65, 405), (65, 440), (450, 440), (450, 450), (50, 450)), 15)
 #lever = poly(((50, 200), (50, 450), (450, 450), (450, 440), (65, 440), (65, 200)))
 lever.color = Color("darkblue")
 lever.elasticity = 0.90
@@ -23,8 +23,16 @@ ball1 = ball((90, 425), 10)
 ball1.color = Color("green")
 ball1.wrap = True
 
+ball4 = ball((110, 425), 10)
+ball4.color = Color("green")
+ball4.wrap = True
+
+ball5 = ball((120, 425), 10)
+ball5.color = Color("green")
+ball5.wrap = True
+
 # bigger ball, initially off the top of the screen
-ball2 = ball((425, -1000), 20)
+ball2 = ball((425, -1000), 20, 5)
 ball2.color = Color("darkgreen")
 
 # bigger ball, farther off the top of the screen
