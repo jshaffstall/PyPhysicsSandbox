c365403899a065ea04a9037aba9c34788611461c
jshaffstall <jshaffstall@gmail.com>
2016-11-26 20:26:31 -0500

    Trying to get car example working

--- Car.py
+++ Car.py
@@ -3,14 +3,29 @@ from pyPhysicsSandbox import *
 WIN_WIDTH = 690
 WIN_HT = 300
 window("Car Test", WIN_WIDTH, WIN_HT)
+gravity(0, 900)
 
 # floor
 base = static_line((-100, 300), (1000, 220), 5)
 base.color = Color("black")
 base.friction = 1.0
 
-base = line((100, 100), (200, 150), 5)
-base.color = Color("black")
-base.friction = 1.0
+wheel1 = ball((45, 200), 25, 100)
+wheel1.color = Color (52, 219, 119)
+wheel1.friction = 1.5
 
-run()
+wheel2 = ball((155, 200), 25, 100)
+wheel2.color = Color (52, 219, 119)
+wheel2.friction = 1.5
+
+chassis = box((75, 160), 50, 30, 100)
+
+pin((100, 200), wheel1, (75, 175), chassis)
+pin((100, 200), wheel1, (75, 215), chassis)
+pin((100, 200), wheel2, (125, 175), chassis)
+pin((100, 200), wheel2, (125, 215), chassis)
+
+motor(wheel1, chassis, 4)
+motor(wheel2, chassis, 4)
+
+run(True)
