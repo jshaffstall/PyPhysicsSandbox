ef75bbb87c4dd753c1b5e5189fe4ba2a3a359f89
jshaffstall <jshaffstall@gmail.com>
2016-11-26 20:32:31 -0500

    Pin joints don't seem to be working in the car example

--- Car.py
+++ Car.py
@@ -9,16 +9,20 @@ gravity(0, 900)
 base = static_line((-100, 300), (1000, 220), 5)
 base.color = Color("black")
 base.friction = 1.0
+base.elasticity = 0.0
 
 wheel1 = ball((45, 200), 25, 100)
 wheel1.color = Color (52, 219, 119)
 wheel1.friction = 1.5
+wheel1.elasticity = 0.0
 
 wheel2 = ball((155, 200), 25, 100)
 wheel2.color = Color (52, 219, 119)
 wheel2.friction = 1.5
+wheel2.elasticity = 0.0
 
 chassis = box((75, 160), 50, 30, 100)
+chassis.elasticity = 0.0
 
 pin((100, 200), wheel1, (75, 175), chassis)
 pin((100, 200), wheel1, (75, 215), chassis)
