1aeba7fd0f4732e26a8d1aafcd813801f2a6a801
jshaffstall <jshaffstall@gmail.com>
2016-12-04 18:17:37 -0500

    Bug fixes

--- TODO.md
+++ TODO.md
@@ -5,6 +5,8 @@
 
 # TODO: Figure out how to do a slip gear
 
+# TODO: function for detecting key presses
+
 # TODO: allow damping and gravity to be specified for each body
 This https://github.com/viblo/pymunk/blob/0d79176cf2fd642bd2ce4005478cb8d6e37c1e9c/examples/breakout.py
 shows setting a custom velocity function on a body.  That's for constant velocity, but might
--- examples/pinball.py
+++ examples/pinball.py
@@ -4,27 +4,30 @@ window('Pinball', 600, 600)
 gravity(0, 900)
 
 lines = []
-lines.append(static_line((450, 500), (550, 50), 1))
-lines.append(static_line((150, 500), (50, 50), 1))
-lines.append(static_line((550, 50), (300, 0), 1))
-lines.append(static_line((300, 0), (50, 50), 1))
-lines.append(static_line((300, 180), (200, 200), 1))
+lines.append(static_line((450, 500), (550, 50), 3))
+lines.append(static_line((150, 500), (50, 50), 3))
+lines.append(static_line((550, 50), (300, 0), 3))
+lines.append(static_line((300, 0), (50, 50), 3))
+#lines.append(static_line((300, 180), (200, 200), 1))
 
 for line in lines:
     line.elasticity = 0.7
     line.group = 1
 
+tri1 = static_triangle((300, 180), (200, 200), (200, 100))
+tri1.elasticity = 1.5
+
 r_pos_x = 150
 r_pos_y = 500
 
-r_flipper = polygon(((r_pos_x-20, r_pos_y-20), (r_pos_x+120, r_pos_y), (r_pos_x-20, r_pos_y+20)), 100)
+r_flipper = polygon(((r_pos_x-20, r_pos_y-20), (r_pos_x+140, r_pos_y), (r_pos_x-20, r_pos_y+20)), 100)
 r_flipper.color = Color('blue')
 r_flipper.group = 1
 
 l_pos_x = 450
 l_pos_y = 500
 
-l_flipper = polygon(((l_pos_x+20, l_pos_y-20), (l_pos_x-120, l_pos_y), (l_pos_x+20, l_pos_y+20)), 100)
+l_flipper = polygon(((l_pos_x+20, l_pos_y-20), (l_pos_x-140, l_pos_y), (l_pos_x+20, l_pos_y+20)), 100)
 l_flipper.color = Color('blue')
 l_flipper.group = 1
 
@@ -41,14 +44,14 @@ l_pivot = pivot((l_pos_x, l_pos_y))
 l_pivot.connect(l_flipper)
 rotary_spring(l_flipper, l_pivot, -0.15, 20000000, 900000)
 
-ball((250,100), 25)
+ball((250, 100), 25)
 ball.elasticity = 0.95
 
 
 def flipper_hit():
     if mouse_pressed():
-        r_flipper.hit((0, -20000), (r_pos_x+100, r_pos_y))
-        l_flipper.hit((0, -20000), (l_pos_x-100, l_pos_y))
+        r_flipper.hit((0, -20000), (r_pos_x+120, r_pos_y))
+        l_flipper.hit((0, -20000), (l_pos_x-120, l_pos_y))
 
 add_observer(flipper_hit)
 
