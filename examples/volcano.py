0d00241a8308eda225391255f099fd97ca25a4c5
jshaffstall <jshaffstall@gmail.com>
2016-12-16 22:10:07 -0500

    Volcano improvements

--- examples/volcano.py
+++ examples/volcano.py
@@ -8,17 +8,18 @@ import random
 
 window("A tiny volcano", 400, 400)
 
-static_line((225, 400), (175, 400), 5)
-static_line((225, 400), (225, 300), 5)
-static_line((175, 300), (175, 400), 5)
-static_line((210, 275), (225, 300), 5)
-static_line((175, 300), (190, 275), 5)
+static_line((225, 400), (175, 400), 15).color=Color('grey')
+static_line((225, 400), (225, 300), 15).color=Color('grey')
+static_line((175, 300), (175, 400), 15).color=Color('grey')
+static_line((220, 275), (225, 300), 15).color=Color('grey')
+static_line((175, 300), (180, 275), 15).color=Color('grey')
 
 # We have to spread the balls out a bit to get uniform expansion,
 # otherwise they all expand horizontally.
-for i in range(150):
+for i in range(500):
     ball1 = ball((200+random.randint(-1,1), 350+random.randint(-1,1)), 5)
-    ball1.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    #ball1.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    ball1.color = Color('red')
 
 run()
 
