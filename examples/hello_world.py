693d4c1e992ebbc35ad865e24fd25e8a789feef2
jshaffstall <jshaffstall@gmail.com>
2016-12-16 20:48:46 -0500

    Added link to hello world screencast

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,6 +1,6 @@
 """
 A traditional Hello World example for PyPhysicsSandbox.  A screencast showing the development
-of this example can be found at:
+of this example can be found at: https://www.youtube.com/watch?v=xux3z2unaME
 """
 
 from pyphysicssandbox import *
@@ -11,7 +11,7 @@ floor = static_box((0, 290), 300, 10)
 floor.color = Color('blue')
 
 caption = text((125, 15), 'Hello World!')
-caption.angle = 270
+caption.angle = 90
 caption.wrap = True
 
 run()
