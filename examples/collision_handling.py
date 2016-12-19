dce8aab2b50790c9f916287ea22ee73beede55f9
jshaffstall <jshaffstall@gmail.com>
2016-12-19 16:51:47 -0500

    Added collision handling example

---
+++ examples/collision_handling.py
@@ -0,0 +1,31 @@
+"""
+An example of collision handling.  The screencast developing this code can be found
+here:
+"""
+
+from pyphysicssandbox import *
+import random
+
+def ball_hits_floor(ball1, floor, p):
+    global count
+
+    ball1.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    #count += 1
+    #if count > 10:
+    #    return False
+
+    return True
+
+window('Collision Handling', 300, 300)
+count = 0
+
+floor = static_box((0, 290), 300, 10)
+floor.color = Color('blue')
+
+ball1 = ball((125, 100), 10, 100)
+ball1.color = Color('green')
+
+add_collision(ball1, floor, ball_hits_floor)
+
+run()
