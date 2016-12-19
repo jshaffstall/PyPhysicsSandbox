cfe9d4f0c8cb6bb4260620b365cf414051ddfaa6
jshaffstall <jshaffstall@gmail.com>
2016-12-19 09:37:31 -0500

    Added observer_functions.py example

---
+++ examples/observer_functions.py
@@ -0,0 +1,29 @@
+"""
+An example of using observer functions.  The screencast developing this code can be found
+here:
+"""
+
+from pyphysicssandbox import *
+
+def my_observer(keys):
+    global ball_point
+
+    if constants.K_b in keys:
+        ball1 = ball(ball_point, 10, 100)
+        ball1.color = Color('green')
+
+    if mouse_clicked():
+        ball_point = mouse_point()
+
+window('Observer Functions', 300, 300)
+
+arm1 = box((100, 150), 100, 10, 100)
+arm1.color = Color("yellow")
+
+pivot1 = pivot((150, 155))
+pivot1.connect(arm1)
+
+ball_point = (125, 50)
+add_observer(my_observer)
+
+run()
