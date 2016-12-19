006fdff6b54a0db55d3e478b719c98c6d92d0835
jshaffstall <jshaffstall@gmail.com>
2016-12-19 17:37:53 -0500

    Added link to gears screencast

--- examples/gears.py
+++ examples/gears.py
@@ -1,15 +1,13 @@
 """
 An example of using gears.  The screencast developing this code can be found
-here:
+here: http://youtu.be/uYDVxDHjjxc?hd=1
 """
 
 from pyphysicssandbox import *
 
 def my_observer(keys):
-    global ball_point
-
     if constants.K_b in keys:
-        ball1 = ball(ball_point, 10, 100)
+        ball1 = ball((125, 50), 10, 100)
         ball1.color = Color('green')
 
 window('Gears', 300, 300)
@@ -20,14 +18,13 @@ arm1.color = Color("yellow")
 pivot1 = pivot((150, 95))
 pivot1.connect(arm1)
 
-ball_point = (125, 50)
-add_observer(my_observer)
-
 arm2 = box((100, 200), 100, 10, 100)
 arm2.color = Color("blue")
 
 pivot2 = pivot((150, 205))
 pivot2.connect(arm2)
+
+add_observer(my_observer)
 
 gear(arm1, arm2)
 
