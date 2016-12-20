27bfeeea5d34bc2f029f320ac1a96b71e4f047fa
jshaffstall <jshaffstall@gmail.com>
2016-12-19 22:13:29 -0500

    Added spring and rotary spring examples

---
+++ examples/rotary_spring.py
@@ -0,0 +1,26 @@
+"""
+An example of using rotary springs.  The screencast developing this code can be found
+here:
+"""
+
+from pyphysicssandbox import *
+
+def my_observer(keys):
+    if constants.K_b in keys:
+        ball1 = ball((125, 50), 10, 100)
+        ball1.color = Color('green')
+
+window('Rotary Springs', 300, 300)
+
+arm1 = box((100, 150), 100, 10, 100)
+arm1.color = Color("yellow")
+
+pivot1 = pivot((150, 155))
+pivot1.connect(arm1)
+
+add_observer(my_observer)
+
+rotary_spring(arm1, pivot1, 0, 50000, 50000)
+#rotary_spring(arm1, pivot1, 0, 20000000, 900000
+
+run()
---
+++ examples/spring.py
@@ -0,0 +1,17 @@
+"""
+An example of using springs on shapes.  The screencast developing this code can be found
+here:
+"""
+
+from pyphysicssandbox import *
+
+window('Springs', 300, 300)
+
+wheel = ball((50, 50), 25)
+wheel.color = Color('blue')
+
+pivot1 = pivot((100, 100))
+
+spring((100, 100), pivot1, (50, 50), wheel, 25, 20000, 1000)
+
+run()
