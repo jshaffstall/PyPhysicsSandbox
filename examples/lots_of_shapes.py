1b78d6eccd4162fb3023a75f7d4a90eca5642539
jshaffstall <jshaffstall@gmail.com>
2016-12-22 12:33:29 -0500

    Added lots of shapes example

--- examples/lots_of_shapes.py
+++ examples/lots_of_shapes.py
@@ -8,14 +8,14 @@ import random
 
 width = 300
 height = 300
-how_many = 2000
+how_many = 5000
 
 window('Lots of Shapes!', width, height)
 
-arm1 = box((100, 145), 100, 10, 100)
+arm1 = box((100, 235), 100, 10, 100)
 arm1.color = Color("yellow")
 
-pivot1 = pivot((150, 150))
+pivot1 = pivot((150, 240))
 pivot1.connect(arm1)
 
 motor(arm1, 5)
@@ -25,7 +25,7 @@ floor = static_box((0, 290), 300, 10)
 for i in range(how_many):
     ball1 = ball((random.randint(0, width), random.randint(0, height)), 5)
     ball1.color = Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    ball1.wrap = True
+    #ball1.wrap = True
 
 
 run ()
